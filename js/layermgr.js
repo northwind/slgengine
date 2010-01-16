f19cb962dc79ac8a91bc57d0c737526e475e00b6
norristong <norristong@718294c2-fd0e-11de-8589-c9554b7fe0d5>
2010-01-16 13:04:26 +0000

    将创建LAYER功能放到PANEL中

--- js/layermgr.js
+++ js/layermgr.js
@@ -3,22 +3,21 @@
  */
 var LayerMgr = Manager.extend({
 	
-	//初始化时记录panel
 	init	: function( panel ){
 		return this._super();
 	},
 	
-	setPanel	: function( panel ){
-		this.panel = panel;
-		return this;
+	setWrap	: function( wrap ){
+		this.wrap = wrap;
 	},
 	
 	reg		: function( level, w, h, layerDiff ){
 		var obj = layerDiff || Layer, 
-			l = new obj( { level : level, ct : this.panel.el } );
-			
-		l.width( w );
-		l.height( h );
+			l = new obj( { level : level, 
+									ct : this.wrap,
+									w	: w,
+									h  : h									
+									 } );
 		
 		this._super( l );
 		
